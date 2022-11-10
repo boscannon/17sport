8c25489e5c8b275cb20692a66da9a58a7f85c943
cannon <cannon>
2022-11-10 18:21:05 +0800

    a

--- resources/views/backend/orders/index.blade.php
+++ resources/views/backend/orders/index.blade.php
@@ -76,7 +76,7 @@ $(function() {
                             <td>${ _.get(item, 'name', '') }</td>
                             <td>${ _.get(item, 'amount', '') }</td>
                         </tr>
-                    `).join("\n") || ''
+                    `).join("\n") || '<tr></tr>'
                 }
             </table>`
         );         

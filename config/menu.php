a71a701a759200897786a27c2977a10abec2009d
cannon <cannon>
2022-11-10 16:17:00 +0800

    q

--- config/menu.php
+++ config/menu.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    [ 'active' => 'products*', 'name' => 'products', 'icon' => 'si si-users', 'title' => 'products', 'routeId' => 'product', 'permissions' => 'products' ],    
-    [ 'active' => 'stock_details*', 'name' => 'stock_details', 'icon' => 'si si-users', 'title' => 'stock_details', 'routeId' => 'stock_detail', 'permissions' => 'stock_details' ],    
-    [ 'active' => 'orders*', 'name' => 'orders', 'icon' => 'si si-users', 'title' => 'orders', 'routeId' => 'order', 'permissions' => 'orders' ],    
+    [ 'active' => 'products*', 'name' => 'products', 'icon' => 'fa fa-product-hunt', 'title' => 'products', 'routeId' => 'product', 'permissions' => 'products' ],    
+    [ 'active' => 'stock_details*', 'name' => 'stock_details', 'icon' => 'fa fa-newspaper-o', 'title' => 'stock_details', 'routeId' => 'stock_detail', 'permissions' => 'stock_details' ],    
+    [ 'active' => 'orders*', 'name' => 'orders', 'icon' => 'fa fa-window-restore', 'title' => 'orders', 'routeId' => 'order', 'permissions' => 'orders' ],    
 
-    [ 'active' => 'departments*', 'name' => 'departments', 'icon' => 'si si-users', 'title' => 'departments', 'routeId' => 'department', 'permissions' => 'departments' ],
-    [ 'active' => 'staff*', 'name' => 'staff', 'icon' => 'si si-users', 'title' => 'staff', 'routeId' => 'staff', 'permissions' => 'staff' ],    
+    [ 'active' => 'departments*', 'name' => 'departments', 'icon' => 'fa fa-group', 'title' => 'departments', 'routeId' => 'department', 'permissions' => 'departments' ],
+    [ 'active' => 'staff*', 'name' => 'staff', 'icon' => 'fa fa-vcard', 'title' => 'staff', 'routeId' => 'staff', 'permissions' => 'staff' ],    
     [ 'active' => 'users*', 'name' => 'users', 'icon' => 'si si-users', 'title' => 'users', 'routeId' => 'user', 'permissions' => 'users' ],
 ];

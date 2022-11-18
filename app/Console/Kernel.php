c4ede19df3702e359c0f94e70d746692b078e350
dennis <a0981038610@gmail.com>
2022-11-18 10:13:44 +0800

    a

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('GetOrders')->everyTenMinutes();
+        $schedule->command('GetOrders')->everyFiveMinutes();
         Log::info('成功排程');
     }
 
